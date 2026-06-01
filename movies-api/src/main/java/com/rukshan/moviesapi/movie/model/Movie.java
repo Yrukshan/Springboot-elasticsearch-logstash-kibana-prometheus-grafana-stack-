bb96080c6c651ccfb0533eb6bae5921493b285a8
Your <yrukshan345@gmail.com>
2026-06-01 13:32:19 +0530

    Spring Boot Backend, Movie.java (model) Commit
    Spring Boot Backend, Movie.java (model) Commit, 2026.06.01

---
+++ movies-api/src/main/java/com/rukshan/moviesapi/movie/model/Movie.java
@@ -0,0 +1,39 @@
+package com.rukshan.moviesapi.movie.model;
+
+import com.rukshan.moviesapi.movie.dto.CreateMovieRequest;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "movies")
+public class Movie {
+
+    @Id
+    private String imdbId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private Short year;
+
+    @Column(nullable = false)
+    private Genre genre;
+
+    @Column(nullable = false)
+    private String country;
+
+    public static Movie from(CreateMovieRequest createMovieRequest) {
+        Movie movie = new Movie();
+        movie.setImdbId(createMovieRequest.imdbId());
+        movie.setTitle(createMovieRequest.title());
+        movie.setYear(createMovieRequest.year());
+        movie.setGenre(createMovieRequest.genre());
+        movie.setCountry(createMovieRequest.country());
+        return movie;
+    }
+}
