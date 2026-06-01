984b093ec797c185f9802c87fb4294fa6e128a04
Your <yrukshan345@gmail.com>
2026-06-01 22:27:17 +0530

    MovieController.java
    Spring Boot Backend 2026.06.01, MovieController.java

---
+++ movies-api/src/main/java/com/rukshan/moviesapi/movie/MovieController.java
@@ -0,0 +1,65 @@
+package com.rukshan.moviesapi.movie;
+
+import com.rukshan.moviesapi.movie.model.Movie;
+import com.rukshan.moviesapi.movie.dto.CreateMovieRequest;
+import com.rukshan.moviesapi.movie.dto.MovieResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+//import static net.logstash.logback.argument.StructuredArguments.kv;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/movies")
+public class MovieController {
+
+    private final MovieService movieService;
+
+    @GetMapping
+    public List<MovieResponse> getMovies() {
+        log.debug("Get all movies");
+        return movieService.getMovies().stream()
+                .map(MovieResponse::from)
+                .toList();
+    }
+
+    @GetMapping("/{imdbId}")
+    public MovieResponse getMovie(@PathVariable("imdbId") String imdbId) {
+        log.debug("Get movie imdbId={}", imdbId);
+        if (imdbId.equals("111")) {
+            throw new NullPointerException("It's known that there is a bug with this movie");
+        }
+        Movie movie = movieService.validateAndGetMovie(imdbId);
+        return MovieResponse.from(movie);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public MovieResponse createMovie(@Valid @RequestBody CreateMovieRequest createMovieRequest) {
+        log.debug("Movie created imdbId={}", createMovieRequest.imdbId());
+        Movie movie = Movie.from(createMovieRequest);
+        movie = movieService.createMovie(movie);
+        return MovieResponse.from(movie);
+    }
+
+    @DeleteMapping("/{imdbId}")
+    public String deleteMovie(@PathVariable("imdbId") String imdbId) {
+        log.debug("Movie deleted imdbId={}", imdbId);
+        Movie movie = movieService.validateAndGetMovie(imdbId);
+        movieService.deleteMovie(movie);
+        return movie.getImdbId();
+    }
+}
